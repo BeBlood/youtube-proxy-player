b88b7f996141cb080d53c67b0a87ad44682d63cb
Benjamin BOUDIER <benjamin.boudier4@gmail.com>
2018-10-19 16:10:23 +0200

    Fix: server.js description & server routes

--- server.js
+++ server.js
@@ -14,21 +14,5 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html', 'index.html', 'text/html', false);
 });
 
-app.get('/controller', (req, res) => {
-    res.sendFile(__dirname + '/controller.html', 'controller.html', 'text/html', false);
-});
-
-app.get('/controller/pilot', (req, res) => {
-    res.sendFile(__dirname + '/controller/pilot.html', 'pilot.html', 'text/html', false);
-});
-
-app.get('/controller/turret', (req, res) => {
-    res.sendFile(__dirname + '/controller/turret.html', 'turret.html', 'text/html', false);
-});
-
-app.get('/controller/engineer', (req, res) => {
-    res.sendFile(__dirname + '/controller/engineer.html', 'engineer.html', 'text/html', false);
-});
-
 httpServer.listen(8080);
 console.log('Server started on port 8080');
